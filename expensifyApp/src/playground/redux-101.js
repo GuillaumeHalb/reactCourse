c05a0d69309ae0ddf573e1ff64ea4a7fcdf183c7
GuillaumeHalb <gha@activeviam.com>
2019-10-17 17:58:51 +0200

    setup redux

---
+++ expensifyApp/src/playground/redux-101.js
@@ -0,0 +1,50 @@
+import { createStore } from "redux";
+
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
+
+console.log(store.getState());
+
+// Actions - than an object that gets ent to the store
+
+// I'd like to increment the count
+store.dispatch({
+  type: "INCREMENT"
+});
+
+console.log(store.getState());
+store.dispatch({
+  type: "INCREMENT"
+});
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "RESET"
+});
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "DECREMENT"
+});
+
+// I'd like to reset the count to zero
+
+console.log(store.getState());
