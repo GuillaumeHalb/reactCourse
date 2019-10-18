01844e74cd21fe7a80b25381facbb6b15888c21f
GuillaumeHalb <gha@activeviam.com>
2019-10-18 15:10:48 +0200

    redux into our app

--- expensifyApp/src/app.js
+++ expensifyApp/src/app.js
@@ -1,7 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
-import AppRouter from "./routers/AppRouter";
+
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpense from "./selectors/expenses";
+
+const store = configureStore();
+
+store.dispatch(
+  addExpense({ description: "Water bill", amount: 100, createdAt: 0 })
+);
+store.dispatch(
+  addExpense({ description: "Gas bill", amount: 300, createdAt: 0 })
+);
+store.dispatch(setTextFilter("bill"));
+
+console.log(
+  getVisibleExpense(store.getState().expenses, store.getState().filters)
+);
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
